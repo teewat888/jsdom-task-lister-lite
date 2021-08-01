5ce6a7383d27406c58cdbada8a5b3f03ab00e442
teerawat prarom <teewat@yahoo.com>
2021-08-01 21:43:50 +1000

    add delete function, prioritydropbox with colour and due date fields

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  document.addEventListener("submit",(e)=>{
+      e.preventDefault();
+      let newTaskDesc = document.getElementById("new-task-description").value;
+      const priority = document.getElementById("priority").value;
+      const duedate = document.getElementById("duedate").value;
+
+      //console.log("new task: ",newTaskDesc);
+      const tasks = document.getElementById("tasks");
+
+      const task = document.createElement("li");
+
+      
+      if (newTaskDesc !== "") {
+        
+        // generate randow id for button 
+        const randomPrefix = Math.floor(Math.random() * 1000) + 1;
+        const bPrefix = randomPrefix+newTaskDesc;
+        // end random number
+
+        switch (priority) {
+          case "High":
+            newTaskDesc = "<span style='color:red'>"+newTaskDesc+"</span>";
+            break;
+          case "Medium":
+            newTaskDesc = "<span style='color:yellow'>"+newTaskDesc+"</span>";
+            break;
+          case "Low":
+            newTaskDesc = "<span style='color:green'>"+newTaskDesc+"</span>";
+            break;
+        }
+
+        task.innerHTML = duedate+" "+newTaskDesc+" <button id='"+bPrefix+"'>X</button>";
+        tasks.appendChild(task);
+        const bx = document.getElementById(bPrefix);
+        bx.addEventListener('click',(e)=>{
+          task.remove();
+        });
+      }
+      
+      //alert(`u press ${newTaskDesc}`);
+  });
 });
